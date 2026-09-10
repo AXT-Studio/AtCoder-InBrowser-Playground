3e756b0a369e77a8fc999f7aad1ec7ca384d1de7
AXT-AyaKoto <148830788+AXT-AyaKoto@users.noreply.github.com>
2026-09-11 05:02:10 +0900

    型周り色々追加

--- entrypoints/main.content/UI/monaco/extraLibs/runtime.ts
+++ entrypoints/main.content/UI/monaco/extraLibs/runtime.ts
@@ -2,10 +2,43 @@
  * Monaco の IntelliSense 用 ambient 宣言。
  * setup.ts から `?raw` で読み込み、addExtraLib に渡す。
  * 拡張本体の tsconfig からは exclude する（DOM の console 等と衝突させない）。
+ *
+ * DOM Libは直接入れると補完が肥大化して場合によってはフリーズしちゃうこともあるっぽいので直接入れはしない。
+ * 実行環境にあって使いそうなものだけここに直接型だけ書いて足しちゃっています。
  */
 declare var console: {
     log(...data: unknown[]): void;
     error(...data: unknown[]): void;
+};
+
+declare function atob(data: string): string;
+declare function btoa(data: string): string;
+
+declare namespace WebAssembly {
+    class Module {
+        constructor(bytes: ArrayBuffer | ArrayBufferView);
+    }
+    class Instance {
+        constructor(module: Module);
+        readonly exports: {
+            readonly [name: string]: (...args: (number | bigint)[]) => number | bigint | void;
+        };
+    }
+}
+
+interface Uint8ArrayConstructor {
+    fromBase64(data: string): Uint8Array;
+    fromHex(data: string): Uint8Array;
+}
+
+interface Uint8Array {
+    toBase64(): string;
+    toHex(): string;
+}
+
+declare var performance: {
+    now(): number;
+    readonly timeOrigin: number;
 };
 
 declare module "fs" {
